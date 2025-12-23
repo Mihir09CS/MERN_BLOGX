05ace17afc1749e90f23c0fd5f079f504ddd5dfc
Mihir Parida <mihirparida15@gmail.com>
2025-12-24 02:59:59 +0530

    Deployment Ready clean Backend fixed 1.0

--- utils/logger.js
+++ utils/logger.js
@@ -1,31 +1,53 @@
-const { createLogger, format, transports } = require("winston");
+// const { createLogger, format, transports } = require("winston");
 
-const logger = createLogger({
+// const logger = createLogger({
+//   level: "info",
+//   format: format.combine(
+//     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+//     format.errors({ stack: true }),
+//     format.splat(),
+//     format.json()
+//   ),
+//   transports: [
+//     new transports.File({
+//       filename: "logs/error.log",
+//       level: "error",
+//     }),
+//     new transports.File({
+//       filename: "logs/combined.log",
+//     }),
+//   ],
+// });
+
+// // Log to console in development
+// if (process.env.NODE_ENV !== "production") {
+//   logger.add(
+//     new transports.Console({
+//       format: format.combine(format.colorize(), format.simple()),
+//     })
+//   );
+// }
+
+// module.exports = logger;
+
+
+const winston = require("winston");
+
+const logger = winston.createLogger({
   level: "info",
-  format: format.combine(
-    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
   ),
   transports: [
-    new transports.File({
-      filename: "logs/error.log",
-      level: "error",
-    }),
-    new transports.File({
-      filename: "logs/combined.log",
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
     }),
   ],
 });
-
-// Log to console in development
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  );
-}
 
 module.exports = logger;
